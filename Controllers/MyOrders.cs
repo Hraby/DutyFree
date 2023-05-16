44e72198928d27370303b0d241cf34627e435053
Honza Běhunčík <honzabehuncik@gmail.com>
2023-05-16 11:22:07 +0200

    Vytvoření MyOrders
    Vytvoření MyOrders, upravení v layoutu

--- Controllers/MyOrders.cs
+++ Controllers/MyOrders.cs
@@ -4,7 +4,13 @@ using DutyFree.Models;
 
 namespace DutyFree.Controllers;
 
-public class myOrders : Controller
+public class MyOrders : Controller
 {
-    
+    private readonly ILogger<HomeController> _logger;
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
 }
