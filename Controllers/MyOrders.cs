1e37593d741c4ace94bac8908b9dc05b26df25fa
Hraby <hrabyyt@gmail.com>
2023-05-16 11:19:34 +0200

    Hotfixes

--- Controllers/MyOrders.cs
+++ Controllers/MyOrders.cs
@@ -4,20 +4,7 @@ using DutyFree.Models;
 
 namespace DutyFree.Controllers;
 
-public class MyOrders : Controller
+public class myOrders : Controller
 {
-    private readonly ILogger<MyOrders> _logger;
-
-    public IActionResult MyOrder()
-    {
-        return View();
-    }
-
-
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View();
-    }
+    
 }
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,15 +1,24 @@
 using DutyFree.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
 
 namespace DutyFree.Controllers;
 
 public class ProductsController : Controller
 {
+    public IActionResult Index()
+    {
+        var ViewModel = new AdminViewModel();
+        ViewModel.Products = _products;
+        return View(ViewModel);
+    }
+    
     private static List<Product> _products = new List<Product>
     {
-        new Product { Name = "Pepsi", Price = 38 },
-        new Product { Name = "Cola", Price = 29 },
-        new Product { Name = "Cola", Price = 29 }
+        new Product { Name = "Pepsi", Price = 20, Quantity = 2},
+        new Product { Name = "Cola", Price = 20, Quantity = 3 },
+        new Product { Name = "Tatranka", Price = 10, Quantity = 5 },
+        new Product { Name = "Čokoláda Milka", Price = 40, Quantity = 3 }
     };
     
     public IActionResult Administration()
@@ -19,5 +28,5 @@ public class ProductsController : Controller
         return View(ViewModel);
     }
 
-    
+    // Todo -> actions: [HttpDelete] delete, [HttpPost] edit
 }
