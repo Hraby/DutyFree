d27bda6d9c9c74b1fe9a475a614cbdd82163efcb
Hraby <hrabyyt@gmail.com>
2023-05-24 09:44:30 +0200

    Order datetime mapping

--- Models/OrderModel.cs
+++ Models/OrderModel.cs
@@ -1,9 +1,14 @@
-﻿namespace DutyFree.Models
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
+
+namespace DutyFree.Models
 {
     public class OrderModel
     {
         public int Id { get; set; }
-        public DateTime DateCreate { get; set; }
+        [Column("DateCreated")]
+        public DateTime? DateCreated { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public int UserId { get; set; }
